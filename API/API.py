465e010dba29207bbf0aa5b495a776ccab858a31
sudhan kandel <sudhankandel03@gmail.com>
2020-03-24 12:44:18 +0545

    last update

--- API/API.py
+++ API/API.py
@@ -1,10 +1,16 @@
-import flask
+import flask,requests
 from flask import request, jsonify
 from TimeSeries import Extract_TimeSeries
 from Detail import Yesterday,Today
+from Nepal import Get_row
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
 daily_data=Extract_TimeSeries()
 yesterday_data=Yesterday()
 today_data=Today()
+nepal=Get_row()
 
 
 app = flask.Flask(__name__)
@@ -24,4 +30,8 @@ def api_yesterday():
 @app.route('/api/today', methods=['GET'])
 def api_Today():
     return jsonify(today_data)
+@app.route('/api/nepal', methods=['GET'])
+def api_nepal():
+    return jsonify(nepal)
+
 app.run()
